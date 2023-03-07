bbf647b380a864841bb928fb8e375c555db0b96e
gsarun125 <127116889+gsarun125@users.noreply.github.com>
2023-03-07 16:03:54 +0530

    Update a.py

--- src/a.py
+++ src/a.py
@@ -1,47 +1,22 @@
-import mediapipe as mp
-import cv2
+import tkinter as tk
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+class UI1:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(master)
+        self.label = tk.Label(self.frame, text="This is UI 1")
+        self.label.pack()
+        self.frame.pack(side="left")
 
-# initialize the webcam
-cap = cv2.VideoCapture(0)
+class UI2:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(master)
+        self.label = tk.Label(self.frame, text="This is UI 2")
+        self.label.pack()
+        self.frame.pack(side="right")
 
-# set the resolution of the webcam
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-# initialize the hand tracking model
-with mp_hands.Hands(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-
-    while True:
-        # read a frame from the webcam
-        ret, frame = cap.read()
-
-        # flip the frame horizontally
-        frame = cv2.flip(frame, 1)
-
-        # convert the frame to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # detect and track the hand in the frame
-        results = hands.process(image)
-
-        # draw the hand landmarks on the frame
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-        # display the frame
-        cv2.imshow('Hand Tracking', frame)
-
-        # check if the user pressed the 'q' key
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-# release the webcam and close the window
-cap.release()
-cv2.destroyAllWindows()
+root = tk.Tk()
+ui1 = UI1(root)
+ui2 = UI2(root)
+root.mainloop()
