e12e6f2961af5e3f1f8b73dca3a2c5fcf698f8e9
DarkShark21 <anderson.vda.avd@gmail.com>
2023-03-20 22:29:15 +0530

    Update vec.py

--- src/vec.py
+++ src/vec.py
@@ -1,3 +1,6 @@
+def lerp(a, b, t):
+    return (t * (b - a)) + a
+
 class vec2:
     def __init__(self, x=0, y=0):
         self.x=x
@@ -8,3 +11,6 @@ class vec2:
     
     def getY(self):
         return self.y
+    
+    def lerp(self, b, t):
+        return vec2(lerp(self.x, b.x, t), lerp(self.y, b.y, t))
